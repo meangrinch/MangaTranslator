1157c283593cb292be57d86ff9b15aa628debc9d
grinnch <srirachaexpress@gmail.com>
2025-06-02 18:36:57 -0400

    Update version

--- core/__init__.py
+++ core/__init__.py
@@ -12,11 +12,11 @@ from .cleaning import clean_speech_bubbles
 from .translation import sort_bubbles_by_reading_order, call_translation_api_batch
 from .rendering import render_text_skia
 
-__version__ = "1.0.3"
-__version_info__ = (1, 0, 3)
+__version__ = "1.0.4"
+__version_info__ = (1, 0, 4)
 __author__ = "grinnch"
 __copyright__ = "Copyright 2025-present grinnch"
-__license__ = "GPL-3.0"
+__license__ = "Apache-2.0"
 __description__ = "A tool for translating manga pages using AI"
 __all__ = [
     "translate_and_render",
