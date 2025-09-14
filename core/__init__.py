59f26bf19d1cb9fd18c5d737adbcb946a4b95eb8
grinnch <srirachaexpress@gmail.com>
2025-09-14 05:09:15 -0400

    Bump version to 1.0.7

--- core/__init__.py
+++ core/__init__.py
@@ -12,8 +12,8 @@ from .cleaning import clean_speech_bubbles
 from .translation import sort_bubbles_by_reading_order, call_translation_api_batch
 from .rendering import render_text_skia
 
-__version__ = "1.0.6"
-__version_info__ = (1, 0, 6)
+__version__ = "1.0.7"
+__version_info__ = (1, 0, 7)
 __author__ = "grinnch"
 __copyright__ = "Copyright 2025-present grinnch"
 __license__ = "Apache-2.0"
